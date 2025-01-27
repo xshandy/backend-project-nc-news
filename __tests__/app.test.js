db2d630a59fc2a80ff0aff27dca7ce3749c9f09a
xshandy <shandygshek@hotmail.co.uk>
2025-01-27 19:28:50 +0000

    add fetchArticleByArticleId function with tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -42,12 +42,57 @@ describe("GET /api/topics", () => {
         });
       });
   });
+});
+
+describe("General error handler - All bad URL", () => {
   test('should respond with 404 and a message of "Endpoint not found" ', () => {
     return request(app)
       .get("/api/NA")
       .expect(404)
       .then((response) => {
         expect(response.body.error).toBe("Endpoint not found");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("should respond with the correct article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        const body = response.body;
+        expect(body.article).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("should send an 400 status and error message when given an invalid id ", () => {
+    return request(app)
+      .get("/api/articles/not-a-number")
+      .expect(400)
+      .then((response) => {
+        const body = response.body.msg;
+
+        expect(body).toBe("Bad Request");
+      });
+  });
+  test("should sends an 404 status and error message when given a valid but non-existent id ", () => {
+    return request(app)
+      .get("/api/articles/1000")
+      .expect(404)
+      .then((response) => {
+        const body = response.body.msg;
+
+        expect(body).toBe("Not found");
       });
   });
 });
