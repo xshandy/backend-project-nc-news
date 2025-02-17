3415fd9d43b9c4412cc0daec28a24dd98d1d9615
xshandy <shandygshek@hotmail.co.uk>
2025-02-17 10:50:05 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const {
   deleteCommentById,
   getUsers,
 } = require("./app.controllers");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", (request, response) => {
